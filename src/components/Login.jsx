a6c1ef3bfc76e2f8b67cf0aa90f9fb5e686f6295
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-01-23 22:20:59 +0600

    login and register component added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,37 @@
 
 
 const Login = () => {
+    const handleLogin = event => {
+        event.preventDefault()
+        const from = event.target;
+        const email = from.email.value;
+        const password = from.password.value;
+        console.log(email, password)
+    }
     return (
-        <div>
-            <h3>Login</h3>
+        <div className="min-h-screen bg-base-200">
+            <div className="card w-3/12 mx-auto shadow-2xl bg-base-100 ">
+                <form onSubmit={handleLogin} className="card-body">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" name="email" placeholder="Your Email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" name="password" placeholder="Your Password" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-secondary">Login</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,38 @@
 
 const Register = () => {
     return (
-        <div>
-            <h3>This is register</h3>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="">
+                <h1 className="text-5xl font-bold mb-10">Register now!</h1>
+                <div className="card w-full shadow-2xl bg-base-100">
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Full Name</span>
+                            </label>
+                            <input type="text" name="name" placeholder="Your Full Name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="Your Email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="Your Password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-secondary">Register</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
